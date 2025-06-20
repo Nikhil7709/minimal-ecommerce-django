86edf55d5836cfd92593b06961651cf0c15bf8df
Nikhil7709 <jadenik13@gmail.com>
2025-06-20 15:53:11 +0530

    feat | added UserAdmin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from store.models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(ImportExportModelAdmin):
+    list_display = ["id", "name", "email", "is_admin", "is_creator"]
+    search_fields = ["name", "email"]
+    list_filter = ["is_admin", "is_creator"]
+    readonly_fields = ["password"]
+    save_as = True
