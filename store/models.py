114455b16af7dee54c7c6ce08f6bde689f3974da
Nikhil7709 <jadenik13@gmail.com>
2025-06-20 15:05:36 +0530

    feat | added Product model

--- store/models.py
+++ store/models.py
@@ -67,3 +67,36 @@ class Category(AbstractAuditCreator, AbstractAuditUpdater):
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "2. Categories"
+
+
+
+class Product(AbstractAuditCreator, AbstractAuditUpdater):
+    """
+    Represents a product available for purchase, including price, stock,
+    and relationships to category and creator (user).
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        blank=True, 
+        null=True
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    stock = models.PositiveIntegerField()
+
+    # F.Ks
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE,
+        related_name="%(app_label)s_%(class)s_category"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "3. Products"
+
+
