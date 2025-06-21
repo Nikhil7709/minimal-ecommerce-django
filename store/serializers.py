f353a33092746435f5cbf38ffda699f01c0cd2d0
Nikhil7709 <jadenik13@gmail.com>
2025-06-21 11:46:15 +0530

    feat | added RegisterSerializer and Login Serializer

---
+++ store/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from store.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "name", "password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
