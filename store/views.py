4e054606cd591e41526d50e0ecdcd61f7267b76d
Nikhil7709 <jadenik13@gmail.com>
2025-06-21 11:59:28 +0530

    feat | write get_tokens_for_user and RegisterAPIView | create app level urls.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from store.serializers import RegisterSerializer
 
 # Create your views here.
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+class RegisterAPIView(APIView):
+    """
+    API view for user registration.
+    Handles user registration by validating input data and creating a new user.
+    """
+    def post(self, request):
+        # Validate the request data using the RegisterSerializer
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = get_tokens_for_user(user)
+            return Response(
+                {
+                    'message': 'Registration successful',
+                    'user': {
+                        'email': user.email,
+                        'name': user.name,
+                    },
+                    'token': token
+                }, 
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
