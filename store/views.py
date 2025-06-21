2d2e044efbc03319eed3fa297cde2a7c76684e0c
Nikhil7709 <jadenik13@gmail.com>
2025-06-21 12:16:57 +0530

    feat | added LoginAPIView | register LoginAPIView in URLs

--- store/views.py
+++ store/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-
-from store.serializers import RegisterSerializer
+from store.serializers import LoginSerializer, RegisterSerializer
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -36,6 +36,34 @@ class RegisterAPIView(APIView):
                     'token': token
                 }, 
                 status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                token = get_tokens_for_user(user)
+                return Response(
+                    {
+                        'message': 'Login successful',
+                        'token': token
+                    },
+                    status=status.HTTP_200_OK
+                )
+            return Response(
+                {
+                    'error': 'Invalid credentials'
+                },
+                status=status.HTTP_401_UNAUTHORIZED
             )
         return Response(
             serializer.errors,
